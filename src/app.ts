62e35dbda88ce19cddfa071ff692b8d19b6d0630
mdsifat <ibnsifat@gmail.com>
2023-11-27 01:34:46 +0600

    basic setup

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app: Application = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Worlds!");
+});
+// error handler
+
+export default app;
